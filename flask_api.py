8d9f57fd211aaa558772c82baee13cc69bbf77f1
IK-R-S <73291742+IK-R-S@users.noreply.github.com>
2021-11-06 12:00:48 -0300

    API

---
+++ flask_api.py
@@ -0,0 +1,44 @@
+import requests
+from flask import Flask, request, jsonify, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>Bem vindo ao exemplo básico de API com Flask!</h1>
+    <p>Nesse exemplo você pode explorar as seguintes rotas, algumas delas com parâmetros:</p>
+    <li><a href="/olamundo">Olá mundo no flask 🌎</a></li>
+    <li><a href="/boasvindas/">Boas vindas dinâmico 😃</a></li>
+    <li><a href="/soma/">Soma de 2 números 🔢</a></li>
+    """
+
+
+@app.route("/olamundo/")
+def hello():
+    return "Olá mundo"
+
+@app.route("/boasvindas/")
+def boasvindas():
+    return "Escreva seu nome no endereço. exemplo: http://127.0.0.1:222/boasvindas/Maria"
+
+@app.route("/boasvindas/<nome>")
+def bemvindo(nome):
+    return f"Olá {nome}, seja bem vindo a API Flask!"
+
+@app.route("/soma/")
+def sum():
+    return """
+    <h2>Para somar escreva os números no endereço da seguinte forma: http://127.0.0.1:222/soma/1/2</h3>
+    
+    <p1>Neste exemplo os número foram 1 e 2; veja com 3 e 4: http://127.0.0.1:222/soma/3/4</p1>
+    """
+
+
+@app.route("/soma/<n1>/<n2>")
+def sumnum(n1, n2):
+    sum = int(n1) + int(n2)
+    return f"SOMA ENTRE {n1} E {n2} = {sum}"
+
+
+app.run(port="222", debug=False)

20bd2bfefc982331ca6adae1dd5ab7fff63a7382
IK-R-S <73291742+IK-R-S@users.noreply.github.com>
2021-11-06 23:42:18 -0300

    Update flask_api.py

--- flask_api.py
+++ flask_api.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, request, jsonify, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -11,6 +11,8 @@ def home():
     <li><a href="/olamundo">Olá mundo no flask 🌎</a></li>
     <li><a href="/boasvindas/">Boas vindas dinâmico 😃</a></li>
     <li><a href="/soma/">Soma de 2 números 🔢</a></li>
+    <li><a href="/jsoncalc/">Resposta em JSON 📃</a></li>
+    <text style="margin-top: 10">by: @IK.R.S</text>
     """
 
 
@@ -40,5 +42,22 @@ def sumnum(n1, n2):
     sum = int(n1) + int(n2)
     return f"SOMA ENTRE {n1} E {n2} = {sum}"
 
+@app.route("/jsoncalc/")
+def jsoncalc():
+    return """
+    <h2>Para somar escreva os números no endereço da seguinte forma: http://127.0.0.1:222/jsoncalc/1/2</h3>
+    
+    <p1>Neste exemplo os número foram 1 e 2; veja com 3 e 4: http://127.0.0.1:222/jsoncalc/3/4</p1>
+    """
 
-app.run(port="222", debug=False)
+@app.route("/jsoncalc/<n1>/<n2>")
+def calc(n1, n2):
+    n1 = int(n1)
+    n2 = int(n2)
+    soma = n1 + n2
+    sub = n1 - n2
+    mult = n1 * n2
+    div = n1 / n2
+    return jsonify({"soma": soma, "subtracao": sub, "multiplicacao": mult, "divisao": div})
+
+app.run(port="4444", debug=False)
